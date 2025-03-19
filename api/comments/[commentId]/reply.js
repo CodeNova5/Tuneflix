f8fb49d7c81f9d1f1bf12895b2e3a45fbb508ae1
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-19 15:01:02 +0100

    Update reply.js

--- api/comments/[commentId]/reply.js
+++ api/comments/[commentId]/reply.js
@@ -69,28 +69,7 @@ export default async function handler(req, res) {
     }
   } 
   
-  if (req.method === 'DELETE') {
-    const { userId } = req.body;
-
-    try {
-      const comment = await Comment.findById(commentId);
-
-      if (!comment) {
-        return res.status(404).json({ message: 'Comment not found.' });
-      }
-
-      if (comment.userId !== userId) {
-        return res.status(403).json({ message: 'You are not authorized to delete this comment.' });
-      }
-
-      await comment.deleteOne();
-
-      return res.status(200).json({ message: 'Comment deleted successfully.' });
-    } catch (error) {
-      console.error('Error deleting comment:', error);
-      return res.status(500).json({ message: 'Internal server error.' });
-    }
-  } 
+  
 
   else {
     return res.status(405).json({ message: 'Method Not Allowed' });
