17a4c3927f7e897081ceb38f4df67012beac9111
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-19 15:04:43 +0100

    Update reply.js

--- api/comments/[commentId]/reply.js
+++ api/comments/[commentId]/reply.js
@@ -38,6 +38,22 @@ const Comment = mongoose.models.Comment || mongoose.model('Comment', commentSche
 export default async function handler(req, res) {
   const { commentId } = req.query;
 
+
+if (req.method === 'GET') {
+    try {
+      const comment = await Comment.findById(commentId).select('replies');
+
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found.' });
+      }
+
+      return res.status(200).json(comment.replies);
+    } catch (error) {
+      console.error('Error fetching replies:', error);
+      return res.status(500).json({ message: 'Internal server error.' });
+    }
+  } 
+
   if (req.method === 'POST') {
     const { content, user, userId, replyTo, commentOwnerId, userImage, fcmtoken, replyId } = req.body;
     
