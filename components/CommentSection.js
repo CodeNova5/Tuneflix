8d808ad2fe14b6d1b4a0faa168d437876a46d800
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-20 12:17:07 +0100

    Update CommentSection.js

--- components/CommentSection.js
+++ components/CommentSection.js
@@ -501,7 +501,7 @@ const showReplies = async (commentId) => {
                 ${reply.image ? `<img class="reply-image" src="${reply.image}" alt="Reply Image" />` : ''}
                 ${reply.video ? `<video class="reply-video" src="${reply.video}" controls></video>` : ''}
                 <div class="reply-actions">
-     <button onClick="editReply('${commentId}', '${reply._id}', '${reply.content}')">Edit</button>  
+    <button onClick={() => editReply(commentId, reply._id, reply.content)}>Edit</button>
 <button onClick="deleteReply('${commentId}', '${reply._id}')">Delete</button>
                 </div>
             `;
