9741cb8cdc73fe848f3f7c96036a418d1d90af1d
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-22 16:22:55 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import FileUpload from '../components/FileUpload';
 import CommentSection from "../components/CommentSection";
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
     <div>
+<Head>
+        <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+      </Head>
+
       <h1>Upload an Image</h1>
       <FileUpload />
       <CommentSection />
