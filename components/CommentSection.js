37f727c97cd8b52fe5208b4e4d34fecc14575a6a
Netdot12 <160973282+Netdot12@users.noreply.github.com>
2025-03-22 17:03:14 +0100

    Update CommentSection.js

--- components/CommentSection.js
+++ components/CommentSection.js
@@ -295,10 +295,11 @@ showReplies(commentId);
     <form id="reply-form" style="display: flex; align-items: center; gap: 8px; padding: 10px; border-top: 1px solid #ccc;">  
        <textarea id="reply-content" type="text" placeholder="Write a reply..." style="flex: 1; padding: 8px; border-radius: 20px; border: 1px solid #ccc; outline: none;"></textarea> 
         <button type="submit" style="background: none; color: white;  border: none; cursor: pointer; font-size: 16px;">  
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style="fill: white;">
-                <path d="M12 2L8 6h3v4h2V6h3l-4-4z"/>
-            </svg>
-        </button>  
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24" style="fill: 	#1F51FF; transform: rotate(50deg);">
+    <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+    <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
+</svg>
+       </button>  
     </form>  
     <p id="reply-error" style="color: red; display: none; padding: 5px;">Error submitting reply. Please try again.</p>  
 `;
