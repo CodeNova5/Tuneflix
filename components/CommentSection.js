0b8dba792772001822ac1a1db15074140d9a0947
Netdot12 <netdot1234@gmail.com>
2025-03-23 12:01:20 +0100

    ggd

--- components/CommentSection.js
+++ components/CommentSection.js
@@ -616,7 +616,7 @@ const CommentSection = () => {
   return (
     <div className={styles.commentSection}>
       <h1 className={styles.commentTitle}>Comment Section</h1>
-      <div style={{ position: "fixed", bottom: "0", left: "0", width: "100%", backgroundColor: "black", display: "flex", alignItems: "center", justifyContent: "space-between", padding: "10px 20px", borderTop: "1px solid #ccc" }}>
+      <div style={{ position: "fixed", bottom: "0", left: "0", width: "100%;", backgroundColor: "black", display: "flex", alignItems: "center", justifyContent: "space-between", padding: "10px 20px", borderTop: "1px solid #ccc" }}>
         <div id="previewContainer" className="previewContainer"></div>
         <label className={styles.commentLabel} onClick={() => imageInputRef.current.click()}>
           <i className="fas fa-image"></i>
@@ -651,11 +651,11 @@ const CommentSection = () => {
         <div className={styles.fileInputContainer}>
 
         </div>
-      <button type="submit">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24" style="fill: #1F51FF; transform: rotate(50deg);">
-        <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
-      </svg>
-    </button>
+        <button type="submit" className={styles.commentSubmit} onClick={postComment} disabled={loading}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24" style={{ fill: "#1F51FF", transform: "rotate(50deg)" }}>
+            <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" />
+          </svg>
+        </button>
       </div>
 
       <div className={styles.commentSection}>
