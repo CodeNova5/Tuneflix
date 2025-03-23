85341259cddf3059113bc93277c887a67cb2c0ff
Netdot12 <netdot1234@gmail.com>
2025-03-23 23:35:25 +0100

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
 import FileUpload from '../components/FileUpload';
 import CommentSection from "../components/CommentSection";
 import Head from 'next/head';
-import SongDetails from '../components/SongDetails';
-
 
 export default function Home() {
   return (
     <div>
-<Head>
-        <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </Head>
 
       <h1>Upload an Image</h1>
       <FileUpload />
       <CommentSection />
-      <SongDetails />
     </div>
   );
 }
