7db617b45cbb57e97ea693db6c750aa1ce89b57d
Netdot12 <netdot1234@gmail.com>
2025-03-27 16:38:36 +0100

    ok

--- api/Music/route.js
+++ api/Music/route.js
@@ -128,7 +128,7 @@ export default async function handler(req, res) {
         const accessToken = await getSpotifyAccessToken();
         const apiUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
           decodedArtistName
-        )}&type=track&limit=20`;
+        )}&type=track&limit=30`;
     
         const response = await fetch(apiUrl, {
           headers: { Authorization: `Bearer ${accessToken}` },
