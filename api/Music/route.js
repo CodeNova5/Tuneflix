4b3cf27cf6742315d9de25e231f4b6857a416ef6
Netdot12 <netdot1234@gmail.com>
2025-04-17 17:03:08 +0100

    small cache practice

--- api/Music/route.js
+++ api/Music/route.js
@@ -511,7 +511,7 @@ export default async function handler(req, res) {
             chartItems.push({ title, artist, image });
           }
         });
-
+        res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
         return res.status(200).json(chartItems);
 
       } catch (error) {
